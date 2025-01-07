16971b21fe5c6c5534a2a6748360aba972801d69
jiyang <330126640@qq.com>
2025-01-07 10:42:41 +0800

    11

---
+++ home/src/main/java/com/jymall/home/HomeApplication.java
@@ -0,0 +1,15 @@
+package com.jymall.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeApplication.class, args);
+    }
+
+}
