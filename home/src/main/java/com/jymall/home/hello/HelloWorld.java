0799b041808a9b9e13f3876c185aca6cc1269329
jiyang <330126640@qq.com>
2025-01-08 13:56:59 +0800

    11

--- home/src/main/java/com/jymall/home/hello/HelloWorld.java
+++ home/src/main/java/com/jymall/home/hello/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.jymall.home.hello;
 
+import com.jymall.home.hello.repository.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HelloWorld {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
+    public String hello() {
+        int count = userMapper.getCount();
+        return "hello world, i have " + count + " 位学生";
     }
 }
